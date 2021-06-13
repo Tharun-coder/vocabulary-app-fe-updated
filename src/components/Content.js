cd5c391d7fbe978e886cc92c575b27c46cd99382
Tharun-coder <tharunecec95@gmail.com>
2021-06-13 16:06:53 +0530

    Initial Commit

---
+++ src/components/Content.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Content(props) {
+  return (
+    <div className="container content-wrapper">
+      <div className="row">
+        <div className="col-lg-6 col-sm-12"></div>
+        <div className="col-lg-6 col-sm-12">
+          <button className="btn-primary">Add Word</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
